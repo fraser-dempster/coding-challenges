5ebbf89344d01fc01d986ae38fee24a51b7eaff8
fraser-dempster <2331229d@student.gla.ac.uk>
2021-12-02 13:27:03 +0000

    finished new problem

---
+++ leetcodeproblems/plusone.java
@@ -0,0 +1,27 @@
+package leetcodeproblems;
+
+import java.util.*;
+
+class Solution {
+  public int[] plusOne(int[] digits) {
+
+    // reverse for loop
+    // add 1 to end of array
+    // if 9 then go back one and increment one there
+    //
+
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+
+    }
+
+    int[] newArray = new int[digits.length + 1];
+    newArray[0] = 1;
+
+    return newArray;
+  }
+}
